a6c5c10ec689cc2d634b658beb7bf7cf0cc41fee
Ubuntu <ubuntu@ip-172-31-30-105.us-east-2.compute.internal>
2020-10-10 13:30:16 +0000

    write tests for flash.js

--- tests/flash.test.js
+++ tests/flash.test.js
@@ -1,5 +1,16 @@
 import flash from '../assets/flash';
 
-it('returns alert message', () => {
-  expect(flash().alertNoPlayers()).toBe('Enter Players');
+jest.mock('../assets/flash', () => ({
+  alertNoPlayers: jest.fn(() => 'Enter Players.'),
+  showPlayers: jest.fn(() => 'Mary and Lucy are playing.'),
+  showWinner: jest.fn(() => 'You win the game.'),
+  showCurrentPlayer: jest.fn(() => 'Mary, your turn.'),
+}));
+
+it('displays game messages', () => {
+  afterEach(() => jest.resetAllMocks());
+  expect(flash.alertNoPlayers()).toBe('Enter Players.');
+  expect(flash.showPlayers()).toBe('Mary and Lucy are playing.');
+  expect(flash.showWinner()).toBe('You win the game.');
+  expect(flash.showCurrentPlayer()).toBe('Mary, your turn.');
 });
